b93e9a5e2ed3149226df382576c607a56af870c3
Ishika <93075613+IshikaCODER@users.noreply.github.com>
2026-09-12 00:00:31 +0530

    Update model.py

--- model.py
+++ model.py
@@ -12,11 +12,6 @@ from scipy import ndimage
 import matplotlib; matplotlib.use("Agg")
 import matplotlib.pyplot as plt, seaborn as sns
 
-IMAGENET_MEAN = [0.485, 0.456, 0.406]
-IMAGENET_STD = [0.229, 0.224, 0.225]
-MEAN_NP = np.array(IMAGENET_MEAN)
-STD_NP = np.array(IMAGENET_STD)
-
 # --- CBAM ---
 class ChannelAttention(nn.Module):
     def __init__(self, ch, r=16):
@@ -41,6 +36,11 @@ class CBAM(nn.Module):
         super().__init__()
         self.ca = ChannelAttention(ch, r); self.sa = SpatialAttention(k)
     def forward(self, x): return self.sa(self.ca(x))
+
+IMAGENET_MEAN = [0.485, 0.456, 0.406]
+IMAGENET_STD = [0.229, 0.224, 0.225]
+MEAN_NP = np.array(IMAGENET_MEAN)
+STD_NP = np.array(IMAGENET_STD)
 
 # --- Superpixel Graph Construction ---
 def _unnorm(t):
