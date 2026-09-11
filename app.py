d18b51e308d0d6a2c735e94451b3bbb0df67d044
Ishika <93075613+IshikaCODER@users.noreply.github.com>
2026-09-12 01:39:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.set_page_config(
     page_title="Brain Tumor Detection & Classification",
     page_icon="🧠",
     layout="wide",
-    initial_sidebar_state="expanded"
+    initial_sidebar_state="collapsed"
 )
 
 # ============================================================
